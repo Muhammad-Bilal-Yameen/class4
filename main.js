783f53b99150ea8d49b29be9d412fd8652ac47ea
muhammad bilal yameen <bilalyameen03151190023@gmail.com>
2024-01-14 23:39:27 -0800

    class4

---
+++ main.js
@@ -0,0 +1,168 @@
+// About if else
+// Exercise # 1
+// let firstName = "Bilal"
+// let lastName = "Ahmed"
+// if(firstName== "Bilal" && lastName=="Ahmed"){
+//     console.log("You are allowed")
+// }
+// else{
+//     console.log("You are not allowed")  // You are allowed / true
+// }
+// Exercise # 2
+// let country = "pakistan"
+// if (country == "pakistani"){
+//     console.log("You can come")
+// }
+// else {
+//     console.log("You can not come")  // You can not come / flase
+// }
+//Exercise # 3
+// let firstName = "Hamza"
+// let lastName = "Syed"
+// if(firstName=== "Hamza" || lastName==="Syed"){
+//     console.log("You are not allowed")
+// }
+// else{
+//     console.log("You are allowed")    // You are not allowed / false
+// }
+// About function
+//Exercise # 4
+// function firstFuncton(){
+//     console.log("my first function")
+//     console.log("my second function")
+// }
+// firstFuncton()
+// Exercise # 5
+//Execsise # 1
+// let firstName = "hamzah"
+// let lastName = "syed"
+// if(firstName === "hamzah" || firstName === "okasah"){
+//     console.log("Your are not allowed")    //ans = you are not allowed
+// }
+// else{
+//     console.log("You are allowed")  
+// }
+// Exercise # 2
+// let firstName = "hamzah"
+// let lastName = "syed"
+// if(firstName === "hamzah" || firstName === "syed"){
+//     console.log("Your are allowed")    //ans = you are allowed
+// }
+// else{
+//     console.log("You are not allowed")  
+// }
+// Exercise # 3
+//Syntex   function funcName () {} 
+// function funcName () {
+//     console.log("my first function")
+//     console.log(123)
+// }
+// funcName()
+// Exercise # 4
+// function funcName (){
+//     return "Bilal"
+// }
+// let name1 = funcName()
+// console.log (name1)  // ans = Bilal
+// Exercise # 5
+// function funcName2 (){
+//     return "syed"
+// }
+// let funcName3 = funcName2()
+//     console.log(funcName3); // ans = syed
+// Exercise # 6
+// function name1 (){
+//     let total:number = 12+12 // ans = 12
+//     return total
+// }
+// let name2 = name1()
+//     console.log(name2);
+// About Arrow Function
+// Exercise # 7
+// syntex = ():string or number or any =>{}
+// let arrowFunction = ():number=>{
+//     let data:number = 123
+// return data;
+// }
+// console.log(arrowFunction());  // ans = 123
+// Exercise # 8
+// function sum() {
+//     console.log(2 + 2);
+// }
+// sum();  // ans = 4
+// Exercise # 9
+// function great() {
+//     console.log("Hello world");
+// }
+//     great();
+// great();
+// great();
+// great();
+// great();
+// great();  / ans = Hello world 5 times
+// Exercise # 10
+// function greet (nmae:string){
+//     console.log("hello word");
+// }
+// greet("hamzah")
+// greet("ali")    //ans = 2 times hello world
+// Exercise # 11
+// function sum (name1:number, name2:number){
+//     console.log(name1*name2);
+// }
+// sum(5,10)   // ans =50
+// Exercise # 12
+// function b1 (num12:number,num13:number){
+//     console.log(num12+num13);
+//     return num12+num13
+// }
+// let ans = b1 (1200,600)  //ans = 1800
+// About arraow method
+//push ()
+//pop ()
+//shift ()
+// unshift ()
+// about push
+// Ecercise # 13
+// let arr:string[] = ["muhammad", "bilal", "yameen"]
+// arr.push("Allah")
+// console.log(arr);  //asn = ['muhammad', 'bilal', 'yameen', 'Allah']
+//Exercise # 14
+// let arr:string[] = ["muhammad", "bilal", "yameen"]
+// console.log(arr); 
+// arr.push("Allah")
+// console.log(arr);  // ans = ['muhammad','bilal', 'yameen']
+//                         // ['muhammad','bilal', 'yameen','Allah']
+// Exercise # 15             
+// let arr:string[] = ["muhammad", "bilal", "thesin", "ahmed"]
+// console.log(arr);
+// console.log(arr.push("sir bilal"));
+// console.log(arr);
+//Pop method
+// Exersice # 16
+// note pop method last wali element delete karta hai.
+// let arr:string[] = ["sir", "miss"]
+// console.log(arr.pop());          // ans = miss
+// shift
+// in pending
+// unshift
+// in pending
+// Exercise # 17
+// let fruit1 = "Apple"
+// let fruit2 = "Banana"
+// let fruit3 = "PineApple"
+// let fruit4 = "Kivi"
+// console.log(fruit1,fruit2,fruit3,fruit4);  //ans = Apple, Banana, PineApple, Kivi
+// Exercise 18
+// let menu =["kofta", "biryani", "kheer","qorma" ]
+// let myFirstPop =menu.pop()
+// console.log(myFirstPop);  // ans = qorma
+// // Exercise # 19
+//0       1         2
+// const fruits = ["banana", "orange", "lemon"]
+// const citrus = fruits.slice(1.2)
+// console.log(citrus);  //ans = ["orange", "lemon"]
+// Exercise # 20
+var fruits = ["bnanaa", "mango", "orange"];
+var a = fruits.splice(2, 0, "lemon", "kivi");
+console.log(a);
